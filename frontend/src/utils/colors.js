c757a9fe5b531d019792767d39d30e45aadc5cf7
Momina Amjad <97848225+mominaamjad@users.noreply.github.com>
2025-04-19 14:50:35 +0500

    modify colorOptions

--- frontend/src/utils/colors.js
+++ frontend/src/utils/colors.js
@@ -1,8 +1,8 @@
 export const colorOptions = [
-  "#FAEDCB",
-  "#C9E4DE",
-  "#C6DEF1",
-  "#DBCDF0",
-  "#F2C6DE",
-  "#F7D9C4",
+  "#267A9E",
+  "#51A885",
+  "#F5A936",
+  "#ED8C37",
+  "#DB7476",
+  "#986B9B",
 ];
